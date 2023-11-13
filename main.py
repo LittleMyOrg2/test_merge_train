919fbf68bc166c8ef2d49b491fd0e1134ae43a1e
nsoulat-gg <139245754+nsoulat-gg@users.noreply.github.com>
2023-11-13 11:13:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # main.py
 
 # connect to website
-username="root"
-password="a74aefd7-59de-438e-8724-49ee6a734e9a"
+username="admin"
+password="a74aefd7-59de-438e-8724-49ee6a734e9b"
 
 if __name__ == "__main__":
   print("Hello World")
