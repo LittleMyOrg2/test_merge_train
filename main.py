a1bccddd9aeb4ac4e99cfe355f22452879677bea
nsoulat-gg <139245754+nsoulat-gg@users.noreply.github.com>
2023-11-14 13:56:03 +0100

    Revert "Update main.py"
    This reverts commit 22b627ec267e4284417cf60fbf64b38b0a073db4.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 # connect to website
 username="admin"
-password="b74aefd7-59de-438e-8724-49ee6a734e9b"
+password="a74aefd7-59de-438e-8724-49ee6a734e9b"
 
 if __name__ == "__main__":
   print("Hello World")
